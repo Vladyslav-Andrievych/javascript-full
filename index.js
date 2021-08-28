9a8fb08480781c0b1e3f6394a6497b34d1634e64
Vladyslav Andrievych <vladandrievych@gmail.com>
2021-08-28 14:13:12 +0300

    lesson 3 3

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
-// code below must be finished ;)
+/* eslint-disable */
 
-/* cast to string */
-console.log('17 to string is ' + String(17));
-console.log('-17.17 to string is ' + String(-17.17));
-console.log('false to string is ' + String(false));
-console.log('null to string is ' + String(null));
-console.log('undefined to string is ' + String(undefined));
-console.log('0 to string is ' + String(0));
+/* Результатом сравнения является булевое значение */
 
-/* cast to number */
-console.log("'17' to number is " + Number('17'));
-console.log('true to number is ' + Number(true));
-console.log('false to number is ' + Number(false));
-console.log('null to number is ' + Number(null));
-console.log('undefined to number is ' + Number(undefined));
-console.log("'   20   ' to number is " + Number('   20   '));
-console.log("'      ' to number is " + Number('      '));
-console.log("'   30d   ' to number is " + Number('  30d  '));
+/* Сравнение чисел */
+const a = 17;
+const b = 5;
+console.log('a > b:', a > b); // true
+console.log('a <= b:', a <= b); //false
+console.log('a == b:', a == b); //false
+console.log('a != b:', a != b); //true
 
-/* cast to boolean */
-console.log('null to boolean is ' + Boolean(null));
-console.log('undefined to boolean is ' + Boolean(undefined));
-console.log('0 to boolean is ' + Boolean(0));
-console.log('-0 to boolean is ' + Boolean(-0));
-console.log('NaN to boolean is ' + Boolean(NaN));
-console.log("'' to boolean is " + Boolean(''));
-console.log("' ' to boolean is " + Boolean(' '));
-console.log('17 to boolean is ' + Boolean(17));
-console.log("'Hello' to boolean is " + Boolean('Hello'));
+/* Сравнение строк */
+console.log("'a' < 'b':", 'a' < 'b'); //true
+console.log("'ab' > 'a':", 'ab' > 'a'); //true
+
+/* При сравнении переменных разных типов они преобразуются в числа */
+console.log("'17' > 1:", '17' > 1); //true
+
+/* Строгое сравнение */
+console.log('17 === 1:', 17 === 1); //false
+console.log('17 === 17:', 17 === 17); //true
+console.log("17 === '17':", 17 === '17'); //false
+console.log('17 === true:', 17 === true); //false
+console.log("'0' === '':", '0' === ''); //false
+console.log('true === false:', true === false); //false
+console.log('true === true:', true === true); //true
+console.log('null === undefined:', null === undefined); //false
+console.log('false === 0:', false === 0); //false
+
+/* Нестрогое сравнение */
+console.log("17 == '17':", 17 == '17'); //true
+console.log("'0' == '':", '0' == ''); //false
+console.log("0 == '':", 0 == ''); //true
+console.log('null == undefined:', null == undefined); //true
+console.log('false == 0:', false == 0); //true
+
+/* Осторожно c null и undefined */
+console.log('undefined == null:', undefined == null); //true
+console.log('undefined == 0:', undefined == 0); //false
+console.log('null == 0:', null == 0); //false
+console.log('undefined < 0:', undefined < 0); // false
+console.log('undefined > 0:', undefined > 0); // false
