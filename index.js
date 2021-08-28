ffffe13e0a3f361149c00452066a1b46a5fbc2d1
Vladyslav Andrievych <vladandrievych@gmail.com>
2021-08-28 17:57:34 +0300

    lesson 4 home 1

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
-/* eslint-disable */
+function run() {
+  var a = 0;
 
-/* Этот код менять не нужно */
-const defaultUserName = 'anonymus';
-const userName = 'John';
-const anotherUser = null;
-const isAdmin = false;
-const message = 'text';
-const anotherMessage = 'text as well';
-const isLoggedIn = Boolean(userName);
+  if (a == 1) {
+    return c;
+  }
 
-/* В коде ниже нужно использовать правильные операторы после знака присваивания */
-const activeUser = anotherUser || defaultUserName;
-const hasAccess = isAdmin || isLoggedIn;
-const isTruthy = message !== anotherMessage;
-const isFalsy = !isLoggedIn;
-const isNotTrue = typeof message === 'number';
-const isTrue = typeof message !== 'boolean';
+  for (let i = 1; i < 10; i++) {
+    console.log(i);
+  }
 
-console.log(activeUser); //'anonymus'
-console.log(hasAccess); //true
-console.log(isTruthy); //true
-console.log(isFalsy); //false
-console.log(isNotTrue); //false
-console.log(isTrue); //true
+  return (d = a);
+}
+
+run();
