c2eab17129cbfe53b67b1a6d6ad15017237f8f71
Vladyslav Andrievych <vladandrievych@gmail.com>
2021-09-06 13:31:14 +0300

    lesson 13 1 to check

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-function getPrimes(num) {
-  for (let i = 2; i <= num; i += 1) {
-    if (i === 2) {
-      console.log(i);
-      continue;
-    }
+const cleanTransactionsList = (transactions) =>
+  transactions
+    .filter((elem) => !isNaN(elem))
+    .map((elem) => '$' + parseFloat(elem).toFixed(2));
 
-    let flag = false;
-
-    for (let j = 2; j < i; j += 1) {
-      if (i % j === 0) {
-        break;
-      } else if (j === i - 1) {
-        flag = true;
-      }
-    }
-
-    if (flag) {
-      console.log(i);
-    }
-  }
-}
-
-getPrimes(62);
-
-// нечетные числа на промежутке
+console.log(
+  cleanTransactionsList(['   1.9 ', '16.4', '17 ', 17, ' 1 dollar '])
+);
