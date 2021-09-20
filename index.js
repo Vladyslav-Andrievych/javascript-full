50b99b0cee321eecba7dcd139b8972122e0becbb
Vladyslav Andrievych <vladandrievych@gmail.com>
2021-09-20 12:36:16 +0300

    lesson 21 homeTask 1

--- index.js
+++ index.js
@@ -1,55 +1,16 @@
-/*
-eslint-disable max-classes-per-file
- */
+export const getTitleElement = () => {
+  const titleElem = document.querySelector('.title');
+  console.dir(titleElem);
+  return titleElem;
+};
 
-class User {
-  constructor(id, name, sessionId) {
-    this._id = id;
-    this._name = name;
-    this._sessionId = sessionId;
-  }
+export const getInputElement = () => {
+  const inputElems = document.querySelectorAll('input');
 
-  get id() {
-    return this._id;
-  }
+  const inputElem = [...inputElems].find(
+    (elem) => elem.getAttribute('type') === 'text'
+  );
 
-  get name() {
-    return this._name;
-  }
-
-  get sessionId() {
-    return this._sessionId;
-  }
-}
-
-class UserRepository {
-  constructor(arrOfUsers) {
-    this._users = arrOfUsers;
-    this._users = Object.freeze(this._users); // без этой строки
-  }
-
-  get users() {
-    return this._users;
-  }
-
-  getUserNames() {
-    return this._users.map(({ _name }) => _name);
-  }
-
-  getUserIds() {
-    return this._users.map(({ _id }) => _id);
-  }
-
-  getUserNameById(id) {
-    return this._users.find(({ _id }) => _id === id)._name;
-  }
-}
-
-const user1 = new User('1', 'Vlad', 'asdf1');
-const user2 = new User('2', 'Vasya', 'asdf2');
-const user3 = new User('3', 'Petya', 'asdf3');
-const user4 = new User('4', 'Nina', 'asdf4');
-
-const userRepository1 = new UserRepository([user1, user2, user3, user4]);
-userRepository1.users = 'str'; // эта строка всё равно не поменяла бы значение свойства .users, так как нету setter
-console.log(userRepository1);
+  console.dir(inputElem);
+  return inputElem;
+};
