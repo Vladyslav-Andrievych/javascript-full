01533726b461378b272cb4eece476d0633a3a94e
Vladyslav Andrievych <vladandrievych@gmail.com>
2021-09-26 14:13:27 +0300

    lesson 24 additional tasks One and Two

--- index.js
+++ index.js
@@ -1,16 +1,53 @@
 const listElem = document.querySelector('.list');
 
 const tasks = [
-  { id: 1, text: 'Buy milk', done: false },
-  { id: 2, text: 'Pick up Tom from airport', done: false },
-  { id: 3, text: 'Visit party', done: false },
-  { id: 4, text: 'Visit doctor', done: true },
-  { id: 5, text: 'Buy meat', done: true },
+  { id: 1, text: 'Buy milk', done: false, time: new Date() },
+  { id: 2, text: 'Pick up Tom from airport', done: true, time: new Date() },
+  { id: 3, text: 'Visit party', done: false, time: new Date() },
+  { id: 4, text: 'Visit doctor', done: true, time: new Date() },
+  { id: 5, text: 'Buy meat', done: false, time: new Date() },
 ];
+
+const compareTime = (a, b) => {
+  const diff = a - b;
+
+  if (diff < 0) {
+    return 1;
+  }
+  if (diff > 0) {
+    return -1;
+  }
+  return 0;
+};
+
+const compareDone = (a, b) => {
+  const diff = a - b;
+
+  if (diff < 0) {
+    return -1;
+  }
+  if (diff > 0) {
+    return 1;
+  }
+  return 0;
+};
 
 const renderTasks = (tasksList) => {
   const tasksElems = tasksList
-    .sort((a, b) => a.done - b.done)
+    .sort((a, b) => {
+      const comapreTimeResult = compareTime(a.time, b.time);
+      const compareDoneResult = compareDone(a.done, b.done);
+
+      if (compareDoneResult) {
+        return compareDoneResult;
+      }
+
+      if (comapreTimeResult) {
+        return comapreTimeResult;
+      }
+
+      return 0;
+    })
     .map(({ id, text, done }) => {
       const listItemElem = document.createElement('li');
       listItemElem.classList.add('list__item');
@@ -31,8 +68,6 @@ const renderTasks = (tasksList) => {
 };
 
 renderTasks(tasks);
-
-// создание дел
 
 const createTaskElem = document.querySelector('.create-task-btn');
 
@@ -57,7 +92,7 @@ const onCreateTask = (event) => {
 
   const id = createId();
 
-  tasks.push({ id, text: taskInputElem.value, done: false });
+  tasks.push({ id, text: taskInputElem.value, done: false, time: new Date() });
 
   taskInputElem.value = '';
 
@@ -66,8 +101,6 @@ const onCreateTask = (event) => {
 };
 
 createTaskElem.addEventListener('click', onCreateTask);
-
-// переключатель выполнено/не выполнено
 
 const onClickCkeckbox = (event) => {
   const isCheckbox = event.target.classList.contains('list__item-checkbox');
@@ -81,8 +114,10 @@ const onClickCkeckbox = (event) => {
 
   if (tasks[taskIndex].done === false) {
     tasks[taskIndex].done = true;
+    tasks[taskIndex].time = new Date();
   } else {
     tasks[taskIndex].done = false;
+    tasks[taskIndex].time = new Date();
   }
 
   listElem.innerHTML = '';
@@ -90,3 +125,67 @@ const onClickCkeckbox = (event) => {
 };
 
 listElem.addEventListener('click', onClickCkeckbox);
+
+// Another Task -----------------------------------------------------------------
+
+// const stringToDate = (string) => {
+//   const [month, date] = string.split('/');
+
+//   return new Date(0, month - 1, date);
+// };
+
+// const compareDates = (a, b) => {
+//   const diff = a - b;
+
+//   if (diff < 0) {
+//     return -1;
+//   }
+
+//   if (diff > 0) {
+//     return 1;
+//   }
+
+//   return 0;
+// };
+
+// const months = [
+//   'Jan',
+//   'Feb',
+//   'Mar',
+//   'Apr',
+//   'May',
+//   'Jun',
+//   'Jul',
+//   'Aug',
+//   'Sep',
+//   'Oct',
+//   'Nov',
+//   'Dec',
+// ];
+
+// const studentsBirthDays = (students) =>
+//   students
+//     .map((student) => ({
+//       name: student.name,
+//       birthDate: stringToDate(student.birthDate),
+//     }))
+//     .sort((student1, student2) =>
+//       compareDates(student1.birthDate, student2.birthDate)
+//     )
+//     .reduce((acc, student) => {
+//       Array.isArray(acc[months[student.birthDate.getMonth()]])
+//         ? acc[months[student.birthDate.getMonth()]].push(student.name)
+//         : (acc[months[student.birthDate.getMonth()]] = [student.name]);
+
+//       return {
+//         ...acc,
+//       };
+//     }, {});
+
+// console.log(
+//   studentsBirthDays([
+//     { name: 'Tom', birthDate: '01/15/2010' },
+//     { name: 'Ben', birthDate: '01/17/2008' },
+//     { name: 'Sam', birthDate: '03/15/2010' },
+//   ])
+// );
